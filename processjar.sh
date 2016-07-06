061d4a3295aa6b4b79badf72688aecfe904f20b4
Matthew Casperson <matthew.casperson@autogeneral.com.au>
2016-07-07 07:44:29 +1000

    Made keystore generation part of the build script

--- processjar.sh
+++ processjar.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+ALIAS=iridium
+PASSWORD=password
+
 ./gradlew clean shadowJar --refresh-dependencies
 mv build/libs/WebAppTesting.jar build/libs/webapptesting-signed.jar
 pack200 --repack build/libs/webapptesting-signed.jar
-jarsigner -keystore webstart/keystore -storepass password -keypass password build/libs/webapptesting-signed.jar wat
+keytool -genkey -noprompt -alias ${ALIAS} -keyalg RSA -keystore keystore -keysize 2048 -storepass ${PASSWORD} -keypass ${PASSWORD}
+jarsigner -keystore keystore -storepass password -keypass ${PASSWORD} build/libs/webapptesting-signed.jar ${ALIAS}
 pack200 build/libs/webapptesting-signed.jar.pack.gz build/libs/webapptesting-signed.jar
+rm keystore
