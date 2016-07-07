72b68dc3045ffaf5cdc646acb486ce18af5df3cd
Matthew Casperson <matthew.casperson@autogeneral.com.au>
2016-07-08 09:24:01 +1000

    Fixing browserstack example

---
+++ examples/16.browserstack/README.md
@@ -0,0 +1,1 @@
+This is an example that demonstrates running tests in BrowserStack.
--- src/main/java/au/com/agic/apptesting/State.java
+++ src/main/java/au/com/agic/apptesting/State.java
@@ -1,8 +1,10 @@
 package au.com.agic.apptesting;
 
 import au.com.agic.apptesting.constants.Constants;
+import au.com.agic.apptesting.utils.SystemPropertyUtils;
 import au.com.agic.apptesting.utils.ThreadWebDriverMap;
 import au.com.agic.apptesting.utils.impl.LocalThreadWebDriverMapImpl;
+import au.com.agic.apptesting.utils.impl.SystemPropertyUtilsImpl;
 import au.com.agic.apptesting.utils.impl.ThreadWebDriverMapImpl;
 
 /**
@@ -12,13 +14,14 @@ import au.com.agic.apptesting.utils.impl.ThreadWebDriverMapImpl;
 public final class State {
 
 	public static final ThreadWebDriverMap THREAD_DESIRED_CAPABILITY_MAP;
+	private static final SystemPropertyUtils SYSTEM_PROPERTY_UTILS = new SystemPropertyUtilsImpl();
 
 	static {
 		/*
 			Select the location of the tests based on the system property
 		 */
 		THREAD_DESIRED_CAPABILITY_MAP = Constants.REMOTE_TESTS.equalsIgnoreCase(
-			System.getProperty(Constants.TEST_DESTINATION_SYSTEM_PROPERTY))
+			SYSTEM_PROPERTY_UTILS.getProperty(Constants.TEST_DESTINATION_SYSTEM_PROPERTY))
 			? new ThreadWebDriverMapImpl()
 			: new LocalThreadWebDriverMapImpl();
 	}
