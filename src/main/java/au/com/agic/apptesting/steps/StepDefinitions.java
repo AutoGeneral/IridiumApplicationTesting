22a1261404059a2b1bc1cf2ebda132980ae7d3fb
Matthew Casperson <matthew.casperson@autogeneral.com.au>
2016-07-13 09:58:39 +1000

    Added some new steps to wait for elements to be clickable

--- src/main/java/au/com/agic/apptesting/steps/StepDefinitions.java
+++ src/main/java/au/com/agic/apptesting/steps/StepDefinitions.java
@@ -469,6 +469,77 @@ public class StepDefinitions {
 	}
 
 	/**
+	 * Waits the given amount of time for an element to be clickable on the page. <p> This is most
+	 * useful when waiting for an element to be in a state where it can be interacted with.
+	 *
+	 * @param waitDuration    The maximum amount of time to wait for
+	 * @param selector        Either ID, class, xpath, name or css selector
+	 * @param alias           If this word is found in the step, it means the selectorValue is found from the data set.
+	 * @param selectorValue   The value used in conjunction with the selector to match the element. If alias was set,
+	 *                        this value is found from the data set. Otherwise it is a literal value.
+	 * @param ignoringTimeout include this text to continue the script in the event that the element can't be found
+	 */
+	@When("^I wait \"(\\d+)\" seconds for (?:a|an|the) element found by (ID|class|xpath|name|css selector)( alias)? "
+			+ "\"([^\"]*)\" to be clickable"
+		+ "(,? ignoring timeouts?)?")
+	public void clickWaitStep(
+			final String waitDuration,
+			final String alias,
+			final String selectorValue,
+			final String ignoringTimeout) throws ExecutionException, InterruptedException {
+		try {
+			SIMPLE_WEB_ELEMENT_INTERACTION.getClickableElementFoundBy(
+				StringUtils.isNotBlank(alias),
+				selectorValue,
+				threadDetails,
+				Long.parseLong(waitDuration)).get();
+		} catch (final TimeoutException ex) {
+			/*
+				Rethrow if we have not ignored errors
+			 */
+			if (StringUtils.isBlank(ignoringTimeout)) {
+				throw ex;
+			}
+		}
+	}
+
+	/**
+	 * Waits the given amount of time for an element to be clickable on the page. <p> This is most
+	 * useful when waiting for an element to be in a state where it can be interacted with.
+	 *
+	 * @param waitDuration    The maximum amount of time to wait for
+	 * @param selector        Either ID, class, xpath, name or css selector
+	 * @param alias           If this word is found in the step, it means the selectorValue is found from the data set.
+	 * @param selectorValue   The value used in conjunction with the selector to match the element. If alias was set,
+	 *                        this value is found from the data set. Otherwise it is a literal value.
+	 * @param ignoringTimeout include this text to continue the script in the event that the element can't be found
+	 */
+	@When("^I wait \"(\\d+)\" seconds for (?:a|an|the) element with "
+		+ "(?:a|an|the) (ID|class|xpath|name|css selector)( alias)? of \"([^\"]*)\" to be clickable"
+		+ "(,? ignoring timeouts?)?")
+	public void clickWaitStep(
+		final String waitDuration,
+		final String selector,
+		final String alias,
+		final String selectorValue,
+		final String ignoringTimeout) {
+		final By by = GET_BY.getBy(selector, StringUtils.isNotBlank(alias), selectorValue, threadDetails);
+		final WebDriverWait wait = new WebDriverWait(
+			threadDetails.getWebDriver(), Integer.parseInt(waitDuration));
+
+		try {
+			wait.until(ExpectedConditions.elementToBeClickable(by));
+		} catch (final TimeoutException ex) {
+			/*
+				Rethrow if we have not ignored errors
+			 */
+			if (StringUtils.isBlank(ignoringTimeout)) {
+				throw ex;
+			}
+		}
+	}
+
+	/**
 	 * Waits the given amount of time for an element to be placed in the DOM. Note that the element does not have to be
 	 * visible, just present in the HTML. <p> This is most useful when waiting for a page to load completely. You can
 	 * use this step to pause the script until some known element is visible, which is a good indication that the page
