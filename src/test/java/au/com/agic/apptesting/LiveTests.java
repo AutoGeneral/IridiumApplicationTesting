a7b7f8a0da62f6280cb963da77e391cac8166d61
Matthew Casperson <matthewcasperson@gmail.com>
2016-08-10 14:44:51 +1000

    Fixing up unit tests

--- src/test/java/au/com/agic/apptesting/LiveTests.java
+++ src/test/java/au/com/agic/apptesting/LiveTests.java
@@ -9,40 +9,31 @@ import org.junit.Test;
 public class LiveTests {
 	@Test
 	public void launchAcceptanceTest() {
-		try {
-			System.setProperty("appURLOverride", "https://mcasperson.github.io/iridium/examples/app.html");
-			System.setProperty("testSource", "https://raw.githubusercontent.com/mcasperson/IridiumApplicationTesting/master/examples/24.acceptancetests/test-populated.feature");
-			System.setProperty("testDestination", "PhantomJS");
-			System.setProperty("enableScenarioScreenshots", "false");
-			System.setProperty("saveReportsInHomeDir", "false");
-			System.setProperty("phantomJSLoggingLevel", "NONE");
+		setCommonProperties();
+		System.setProperty("appURLOverride", "https://mcasperson.github.io/iridium/examples/app.html");
+		System.setProperty("testSource", "https://raw.githubusercontent.com/mcasperson/IridiumApplicationTesting/master/examples/24.acceptancetests/test-populated.feature");
+		System.setProperty("testDestination", "PhantomJS");
 
-			Assert.assertEquals(0, new TestRunner().run());
-		} finally {
-			resetDriverPaths();
-		}
+		Assert.assertEquals(0, new TestRunner().run());
 	}
 
 	@Test
 	public void launchVerificationTest() {
-		try {
-			System.setProperty("appURLOverride", "http://ticketmonster-jdf.rhcloud.com");
-			System.setProperty("testSource", "https://raw.githubusercontent.com/mcasperson/IridiumApplicationTesting/master/examples/22.verification/test.feature");
-			System.setProperty("testDestination", "Firefox");
-			System.setProperty("enableScenarioScreenshots", "false");
-			System.setProperty("saveReportsInHomeDir", "false");
-			System.setProperty("phantomJSLoggingLevel", "NONE");
+		setCommonProperties();
+		System.setProperty("appURLOverride", "http://ticketmonster-jdf.rhcloud.com");
+		System.setProperty("testSource", "https://raw.githubusercontent.com/mcasperson/IridiumApplicationTesting/master/examples/22.verification/test.feature");
+		System.setProperty("testDestination", "Firefox");
 
-			Assert.assertEquals(0, new TestRunner().run());
-		} finally {
-			resetDriverPaths();
-		}
+		Assert.assertEquals(0, new TestRunner().run());
 	}
 
-	private void resetDriverPaths() {
+	private void setCommonProperties() {
 		System.setProperty("webdriver.chrome.driver", "");
 		System.setProperty("webdriver.opera.driver", "");
 		System.setProperty("webdriver.ie.driver", "");
 		System.setProperty("phantomjs.binary.path", "");
+		System.setProperty("enableScenarioScreenshots", "false");
+		System.setProperty("saveReportsInHomeDir", "false");
+		System.setProperty("phantomJSLoggingLevel", "NONE");
 	}
 }
