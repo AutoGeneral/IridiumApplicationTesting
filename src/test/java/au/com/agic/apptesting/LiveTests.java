24b7470487e3a8d8f769db0fcffa3a109f7590e7
Matthew Casperson <matthewcasperson@gmail.com>
2016-08-10 13:13:01 +1000

    Added a new test

--- src/test/java/au/com/agic/apptesting/LiveTests.java
+++ src/test/java/au/com/agic/apptesting/LiveTests.java
@@ -6,12 +6,24 @@ import org.junit.Test;
 /**
  * Runs the acceptance test example
  */
-public class AcceptanceTests {
+public class LiveTests {
 	@Test
 	public void launchAcceptanceTest() {
 		System.setProperty("appURLOverride", "https://mcasperson.github.io/iridium/examples/app.html");
 		System.setProperty("testSource", "https://raw.githubusercontent.com/mcasperson/IridiumApplicationTesting/master/examples/24.acceptancetests/test-populated.feature");
 		System.setProperty("testDestination", "PhantomJS");
+		System.setProperty("enableScenarioScreenshots", "false");
+		System.setProperty("saveReportsInHomeDir", "false");
+		System.setProperty("phantomJSLoggingLevel", "NONE");
+
+		Assert.assertEquals(0, new TestRunner().run());
+	}
+
+	@Test
+	public void launchVerificationTest() {
+		System.setProperty("appURLOverride", "http://ticketmonster-jdf.rhcloud.com");
+		System.setProperty("testSource", "https://raw.githubusercontent.com/mcasperson/IridiumApplicationTesting/master/examples/22.verification/test.feature");
+		System.setProperty("testDestination", "Firefox");
 		System.setProperty("enableScenarioScreenshots", "false");
 		System.setProperty("saveReportsInHomeDir", "false");
 		System.setProperty("phantomJSLoggingLevel", "NONE");
