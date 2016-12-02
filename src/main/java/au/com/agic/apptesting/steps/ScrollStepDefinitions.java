08d40cad3174821b44c4eb5ca7d9ae746f8e1497
Matthew Casperson <matthew.casperson@autogeneral.com.au>
2016-12-02 10:40:48 +1000

    Added scrolling steps

---
+++ src/main/java/au/com/agic/apptesting/steps/ScrollStepDefinitions.java
@@ -0,0 +1,51 @@
+package au.com.agic.apptesting.steps;
+
+import au.com.agic.apptesting.State;
+import au.com.agic.apptesting.utils.AutoAliasUtils;
+import au.com.agic.apptesting.utils.SleepUtils;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import cucumber.api.java.en.When;
+
+/**
+ * Gherin steps used to scroll the web page
+ *
+ * These steps have Atom snipptets that start with the prefix "scroll".
+ * See https://github.com/mcasperson/iridium-snippets for more details.
+ */
+@Component
+public class ScrollStepDefinitions {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrollStepDefinitions.class);
+	@Autowired
+	private SleepUtils sleepUtils;
+	@Autowired
+	private AutoAliasUtils autoAliasUtils;
+
+
+	/**
+	 * Scrolls to the bottom of the page
+	 */
+	@When("^I scroll to the bottom of the page$")
+	public void scrollToBottom() {
+		final WebDriver webDriver = State.THREAD_DESIRED_CAPABILITY_MAP.getWebDriverForThread();
+		final JavascriptExecutor js = JavascriptExecutor.class.cast(webDriver);
+		js.executeScript("window.scrollTo(0, document.body.scrollHeight");
+	}
+
+	/**
+	 * Scrolls to the bottom of the page
+	 */
+	@When("^I scroll to the top of the page$")
+	public void scrollToTop() {
+		final WebDriver webDriver = State.THREAD_DESIRED_CAPABILITY_MAP.getWebDriverForThread();
+		final JavascriptExecutor js = JavascriptExecutor.class.cast(webDriver);
+		js.executeScript("window.scrollTo(0, 0");
+	}
+}
