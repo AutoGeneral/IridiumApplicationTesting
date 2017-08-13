7626bd3db680736c142315898055c55b2214f541
Matthew Casperson <matthewcasperson@gmail.com>
2017-08-13 18:16:05 +1000

    Increased the timeout while waiting for a video from Browserstack

--- src/main/java/au/com/agic/apptesting/utils/impl/RetryServiceImpl.java
+++ src/main/java/au/com/agic/apptesting/utils/impl/RetryServiceImpl.java
@@ -2,7 +2,7 @@ package au.com.agic.apptesting.utils.impl;
 
 import au.com.agic.apptesting.constants.Constants;
 import au.com.agic.apptesting.utils.RetryService;
-import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
@@ -14,8 +14,8 @@ public class RetryServiceImpl implements RetryService {
 		final SimpleRetryPolicy policy = new SimpleRetryPolicy();
 		policy.setMaxAttempts(Constants.WEBDRIVER_ACTION_RETRIES);
 
-		final FixedBackOffPolicy backoff = new FixedBackOffPolicy();
-		backoff.setBackOffPeriod(2000l);
+		final ExponentialBackOffPolicy backoff = new ExponentialBackOffPolicy();
+		backoff.setInitialInterval(5000);
 
 		final RetryTemplate template = new RetryTemplate();
 		template.setRetryPolicy(policy);
