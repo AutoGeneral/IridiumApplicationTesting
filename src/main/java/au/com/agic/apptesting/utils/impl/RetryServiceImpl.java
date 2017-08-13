a46935e2c0f93d9be1a572028185a7a8503c00b6
Matthew Casperson <matthewcasperson@gmail.com>
2017-08-13 17:24:35 +1000

    Made mobile tests separate, because they take so long

--- src/main/java/au/com/agic/apptesting/utils/impl/RetryServiceImpl.java
+++ src/main/java/au/com/agic/apptesting/utils/impl/RetryServiceImpl.java
@@ -2,6 +2,7 @@ package au.com.agic.apptesting.utils.impl;
 
 import au.com.agic.apptesting.constants.Constants;
 import au.com.agic.apptesting.utils.RetryService;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,15 @@ import org.springframework.stereotype.Component;
 public class RetryServiceImpl implements RetryService {
 	@Override
 	public RetryTemplate getRetryTemplate() {
-		final RetryTemplate template = new RetryTemplate();
 		final SimpleRetryPolicy policy = new SimpleRetryPolicy();
 		policy.setMaxAttempts(Constants.WEBDRIVER_ACTION_RETRIES);
+
+		final FixedBackOffPolicy backoff = new FixedBackOffPolicy();
+		backoff.setBackOffPeriod(2000l);
+
+		final RetryTemplate template = new RetryTemplate();
 		template.setRetryPolicy(policy);
+		template.setBackOffPolicy(backoff);
 		return template;
 	}
 }
