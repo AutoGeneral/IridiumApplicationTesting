0227c3c48bc60fae3e2a0ae251b6e4310906e7cc
Matthew Casperson <matthewcasperson@gmail.com>
2017-10-19 19:03:20 +1000

    Added AWS Lambda entry point

---
+++ src/main/java/au/com/agic/apptesting/lambda/LambdaEntry.java
@@ -0,0 +1,18 @@
+package au.com.agic.apptesting.lambda;
+
+import au.com.agic.apptesting.Main;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+/**
+ * An entry point for the app when called from AWS lambda
+ */
+public class LambdaEntry implements RequestHandler<String, String> {
+
+	@Override
+	public String handleRequest(String input, Context context) {
+		final int failures = Main.run();
+		return "Failure count: " + failures;
+	}
+
+}
